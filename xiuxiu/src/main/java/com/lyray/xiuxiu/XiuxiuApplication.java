23a5e5f09b0b126956a4ad674b54800034793be0
Lyray <Lyray@lyray.com>
2015-07-01 18:42:53 +0800

    Init commit.

---
+++ xiuxiu/src/main/java/com/lyray/xiuxiu/XiuxiuApplication.java
@@ -0,0 +1,12 @@
+package com.lyray.xiuxiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiuxiuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiuxiuApplication.class, args);
+    }
+}
---
+++ xiuxiu/src/test/java/xiuxiu/XiuxiuApplicationTests.java
@@ -0,0 +1,20 @@
+package xiuxiu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.lyray.xiuxiu.XiuxiuApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = XiuxiuApplication.class)
+@WebAppConfiguration
+public class XiuxiuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
