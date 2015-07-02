8aea32c9e492bf3f1f986e633f6e12a1862f3621
hzlihui2014 <hzlihui2014@corp.netease.com>
2015-07-02 22:38:37 +0800

    Update dockerfile and security.

---
+++ xiuxiu/src/main/java/com/lyray/xiuxiu/controller/IndexController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.lyray.xiuxiu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author hzlihui2014
+ *
+ */
+@Controller
+public class IndexController {
+
+	@RequestMapping("/index")
+	public @ResponseBody String index(){
+		return "Hello world!";
+	}
+	
+}
